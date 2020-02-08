0a7fac76c63a88fe4bf30c5b548c0fa98acc98a5
jepidoptera <jepidoptera@live.com>
2020-02-08 13:17:36 -0600

    misspelling

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("view engine", ".hbs");
 console.log('Listening on: http://localhost:' + port);
 
 app.get ("/", (req, res) => {
-    res.render("inddex");
+    res.render("index");
 })
 
 app.get('/game/:levelId', (req, res) => {
